5bdb7ce1e8fce8bf80015a3b6ecc4018e14bd1a6
rishabh <dsvvian.rishabh@gmail.com>
2017-08-18 22:39:06 +0530

    offers

---
+++ api/controllers/OffersController.js
@@ -0,0 +1,11 @@
+/**
+ * OffersController
+ *
+ * @description :: Server-side logic for managing offers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
